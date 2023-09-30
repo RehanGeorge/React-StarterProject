2085cdcabcc4f32376b0ee006103283e310f32e5
RehanGeorge <rehan.george@gmail.com>
2023-09-30 22:54:16 +0530

    Added NewExpense components

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,8 @@
 import Expenses from "./components/Expenses/Expenses";
 
+import ExpenseForm from "./components/NewExpense/ExpenseForm";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 const App = () => {
   const expenses = [
     {
@@ -25,7 +28,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
